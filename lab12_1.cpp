638a3358d4ba6f6eaa5fe1240ce39f7f8902aa0a
JayAleecha <aleecha.2546@gmail.com>
2023-01-06 16:12:39 +0700

    lab12_1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,50 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<ctime>
+using namespace std;
+int main(){
+ cout << "Press Enter 3 times to reveal your future.";
+ cin.get();
+ cin.get();
+ cin.get();
+ string x;
+ srand(time(0));
+ int i=rand()%9+1;
+ if(i==1){
+  x="A";
+  }else{
+  if(i==2){
+   x="B+";
+   }else{
+   if(i==3){
+    x="B";
+    }else{
+    if(i==4){
+     x="C+";
+     }else{
+     if(i==5){
+      x="C";
+      }else{
+      if(i==6){
+       x="D+";
+       }else{
+       if(i==7){
+        x="D";
+        }else{
+        if(i==8){
+         x="F";
+         }else{
+          x="W";
+          }
+         }
+       }
+      }
+     }
+    }
+   }
+  }
+
+
+ cout << "You will get " << x << " in this 261102.";
+ return 0;
+}
